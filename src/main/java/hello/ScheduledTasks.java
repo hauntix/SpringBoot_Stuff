94f24c3b02e1c17e60b376c14df156cfb9ae81d1
hauntix <c.estrada@email.com>
2019-10-21 08:56:01 -0400

    Initial commit

---
+++ src/main/java/hello/ScheduledTasks.java
@@ -0,0 +1,23 @@
+package hello;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+
+	private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+	// every 5 seconds do this task
+	@Scheduled(fixedRate = 60000)
+	public void reportCurrentTime() {
+		log.info("The time is now {}", dateFormat.format(new Date()));
+	}
+}
